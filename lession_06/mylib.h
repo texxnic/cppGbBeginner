34e7cd7e33c7524f26b2745d72a1cb054895e0d2
Roman Balan <texxnic@gmail.com>
2021-10-17 12:45:30 +0300

    lession6

---
+++ lession_06/mylib.h
@@ -0,0 +1,6 @@
+void initalizeArr(float *arr, int *arrSize);
+void printFloatArray(float *arr, int *arrSize);
+void printIntArray(int *arr, int *arrSize);
+void printPosAndNegativeCount(float *arr, int *arrSize);
+void swapINT(int *xp, int *yp);
+void bubbleSort(int arr[], int n);
